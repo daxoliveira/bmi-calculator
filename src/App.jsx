f252f83e1283032ab3dedd6d5bf5e7ccde86340c
Dax Oliveira <dax.oliveira.br@gmail.com>
2023-12-12 12:20:27 -0600

    implement conditional rendering for measuring system

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,76 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [measuringSystem, setMeasuringSystem] = useState('metric');
+  function onSystemChange(e) {
+    console.log(e);
+    if (e.target.value === 'metric') {
+      setMeasuringSystem('metric');
+    }
+    if (e.target.value === 'imperial') {
+      setMeasuringSystem('imperial');
+    }
+  }
 
   return (
     <>
-      <form>
-
       <h3>
         Enter your details below
       </h3>
-        <label>
-          <input 
-            type='radio' 
-            name='metric' 
-            value='metric'
-            defaultChecked={true}
-            />
-          Metric
-        </label>
-        <label>
-          <input 
-            type='radio' 
-            name='imperial' 
-            value='imperial'
-            />
-          Imperial
-        </label>
-
-        <label>
-          Height
-          <input 
-            type='text' 
-            name='height' 
-            placeholder='180'
-            />
-        </label>
-
-        <label>
-          Weight
-          <input 
-            type='text' 
-            name='weight' 
-            placeholder='80'
-            />
-        </label>
-
-        <div>
-          <h4>Your BMI is...</h4>
-          <h3>23.4</h3>
-          <p>Your BMI suggests you're a healthy weight. Your ideal weight is between <strong>63.3kgs - 85.2kgs</strong>.</p>
-        </div>
-
-      </form>
+      <label>
+      <input type='radio' name='measuringSystem' value='metric' onChange={onSystemChange} defaultChecked={true} />
+        Metric
+      </label>
+      <label>
+        <input type='radio' name='measuringSystem' value='imperial' onChange={onSystemChange} />
+        con
+        Imperial
+      </label>
+      {measuringSystem === 'metric' && <Metric/>}
+      {measuringSystem === 'imperial' && <Imperial/>}
+      <div>
+        <h4>Your BMI is...</h4>
+        <h3>23.4</h3>
+        <p>Your BMI suggests you&apos;re a healthy weight. Your ideal weight is between <strong>63.3kgs - 85.2kgs</strong>.</p>
+      </div>
     </>
   )
 }
 
-export default App
+export function Metric() {
+  return (
+    <form>
+      <label>
+        Height
+        <br />
+        <input type='text' name='height' placeholder='185cm'/>
+      </label>
+      <br />
+      <label>
+        Weight
+        <br />
+        <input type='text' name='weight' placeholder='80kg'/>
+      </label>
+    </form>
+  )
+}
+
+export function Imperial() {
+  return (
+    <form>
+      <label>
+        Height
+        <br />
+        <input type='text' name='feet' placeholder='5 ft' defaultValue='11'/>
+        <input type='text' name='inches' placeholder='11 in' defaultValue='11'/>
+      </label>
+      <br />
+      <label>
+        Weight
+        <br />
+        <input type='text' name='stones' placeholder='11 st'/>
+        <input type='text' name='pounds' placeholder='4 lbs'/>
+      </label>
+    </form>
+  )
+}
